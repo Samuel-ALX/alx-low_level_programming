bd10d09d50a32826aa01fe5e529cd22714a11ee4
Samuel <eaglesamchuks1@gmail.com>
2023-02-21 11:52:45 -0800

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_fib - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * @len: the length of with the fib sequance should print to.
+ *
+ * Return: void
+ */
+void print_fib(int len)
+{
+
+}
+
+
+/**
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	print_fib(98);
+	return (0);
+}
