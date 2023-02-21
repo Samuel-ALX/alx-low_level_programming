c228d341250ebaf5b7c3c13d5d6c312e32c0c67e
Samuel <eaglesamchuks1@gmail.com>
2023-02-21 11:46:50 -0800

    first 100 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_fib_x50 - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: void
+ */
+void print_fib_x50(void)
+{
+
+}
+
+
+
+/**
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	print_fib_x50();
+	return (0);
+}
+
