e965950f6628c8a8ae7bcfa858241b2e488b507d
Samuel <eaglesamchuks1@gmail.com>
2023-02-22 07:54:27 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,9 +8,24 @@
  */
 void print_fib_x50(void)
 {
+	unsigned int target;
+	unsigned long int f, l, i, fib;
 
+	f = 1;
+	l = 2;
+	target = 50 - 2;
+	printf("1, 2, ");
+	for (i = 0 ; i < target ; i++)
+	{
+		fib = f + l;
+		printf("%lu", fib);
+		f = l;
+		l = fib;
+		if (i < target - 1)
+			printf(", ");
+	}
+	putchar('\n');
 }
-
 
 
 /**
@@ -24,4 +39,3 @@ int main(void)
 	print_fib_x50();
 	return (0);
 }
-
