37b05236dfe25cd89c2d3390333d9721c1136607
Samuel <eaglesamchuks1@gmail.com>
2023-02-27 05:06:52 -0800

    I do not fear computers.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string .
- * @str: string.
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
-
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
