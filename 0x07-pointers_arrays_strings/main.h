b9d1ce404bcb4e9570918a7e9da05dc2c783f10f
Samuel <eaglesamchuks1@gmail.com>
2023-03-06 13:09:30 -0800

    Even more pointers, arrays and strings C

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 
 #endif /*main.h*/
