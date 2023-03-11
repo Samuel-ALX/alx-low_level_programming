d0192fb0f6a94aed212962ff2d60cb6ea8a9e8aa
Samuel <eaglesamchuks1@gmail.com>
2023-03-10 16:18:30 -0800

    C - argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int is_integer(const char *str);
+int get_coin_number(int amount, int nb_coins);
+
+#endif
